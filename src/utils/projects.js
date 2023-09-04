49a70bb5513627961ce43627bb7a08a46b4d0c6b
Jalen Murray <jalenmmurray@gmail.com>
2023-09-04 12:44:57 -0400

    Added PokemonWikiReact Project

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,5 +1,6 @@
 import P1 from '../assets/ecommerce-project.png';
 import P2 from '../assets/pokemon-wiki.png';
+import P3 from '../assets/pokemon-wiki-react.png';
 
 export const PROJECTS = [
   {
@@ -13,10 +14,19 @@ export const PROJECTS = [
   {
     name: 'Pokemon Wiki',
     image: P2,
-    skills: 'React,Node.js,Firebase,Bootstrap',
+    skills: 'Node.js,Javascript,Bootstrap,CSS',
     github: 'https://github.com/JalenMurray/Pokemon-Wiki',
     link: 'https://jalenmurray.github.io/Pokemon-Wiki/',
     description: 'Provides information on all using the PokeAPI. Styled cards used to display the information',
+  },
+  {
+    name: 'Pokemon Wiki React',
+    image: P3,
+    skills: 'React,Node.js,Bootstrap,Styled-Components',
+    github: 'https://github.com/JalenMurray/pokemon-wiki-react',
+    link: 'https://main--astonishing-tarsier-248c4f.netlify.app/',
+    description:
+      'Provides information on all using the PokeAPI. Styled cards used to display the information (Similar to other Pokemon Wiki Currently, just uses React.  Hope to add more features in the future)',
   },
   {
     name: 'Absence Management System',
